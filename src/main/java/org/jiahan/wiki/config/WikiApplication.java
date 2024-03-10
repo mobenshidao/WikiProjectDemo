7c8d8887cbca5f0bfa39503943f3e1f2be7e1fb2
jiahan.han <jiahan.han527@gmail.com>
2024-03-10 17:21:33 +0800

    2-6 add hello world mouthpiece

--- src/main/java/org/jiahan/wiki/config/WikiApplication.java
+++ src/main/java/org/jiahan/wiki/config/WikiApplication.java
@@ -1,11 +1,13 @@
-package org.jiahan.wiki;
+package org.jiahan.wiki.config;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
 import org.springframework.core.env.Environment;
 
+@ComponentScan("org.jiahan.wiki")
 @SpringBootApplication
 public class WikiApplication {
 
---
+++ src/main/java/org/jiahan/wiki/controller/TestController.java
@@ -0,0 +1,15 @@
+package org.jiahan.wiki.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class TestController {
+
+    @GetMapping("/hello")
+    public String hello(){
+
+        return "hello world!";
+
+    }
+}
