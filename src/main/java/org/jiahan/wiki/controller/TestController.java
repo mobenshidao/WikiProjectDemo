8ee432faefe64732dce6a5d5cc6cb82059076576
jiahan.han <jiahan.han527@gmail.com>
2024-03-10 17:42:54 +0800

    2-8 add default application.properties

--- src/main/java/org/jiahan/wiki/controller/TestController.java
+++ src/main/java/org/jiahan/wiki/controller/TestController.java
@@ -1,5 +1,6 @@
 package org.jiahan.wiki.controller;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -7,10 +8,13 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class TestController {
 
+    @Value("${test.hello:Test}")
+    private String testHello;
+
     @GetMapping("/hello")
     public String hello(){
 
-        return "Hello World!";
+        return "Hello World!" + testHello;
 
     }
 
