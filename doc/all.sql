56d87d42676a150de123fe3887574d5dfb77d73e
jiahan.han <jiahan.han527@gmail.com>
2024-03-18 21:26:00 +0800

    3-5 add mybatis generator and try  generate demo

--- doc/all.sql
+++ doc/all.sql
@@ -8,3 +8,14 @@ create table `wikitest`(
 )engine = innodb default charset = utf8mb4 comment = 'wikitest';
 
 insert into `wikitest` (id,name,password) value (1, 'test','testpassword');
+
+
+drop table if exists `demo`;
+
+create table `demo`(
+                           `id` bigint not null comment 'id',
+                           `name` varchar(50) comment 'name',
+                            primary key(`id`)
+)engine = innodb default charset = utf8mb4 comment = 'demo';
+
+insert into `demo` (id,name) value (1, 'test');
