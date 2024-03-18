e8aa5f5a9e9bca803273f32f26a1557b60dcbc0e
jiahan.han <jiahan.han527@gmail.com>
2024-03-19 00:04:50 +0800

    3-6-3 add CommonResp class and modify resp information

--- src/main/java/org/jiahan/wiki/controller/EbookController.java
+++ src/main/java/org/jiahan/wiki/controller/EbookController.java
@@ -2,6 +2,7 @@ package org.jiahan.wiki.controller;
 
 import jakarta.annotation.Resource;
 import org.jiahan.wiki.domain.Ebook;
+import org.jiahan.wiki.resp.CommonResp;
 import org.jiahan.wiki.service.EbookService;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -17,9 +18,15 @@ public class EbookController {
     private EbookService ebookService;
 
     @GetMapping("/list")
-    public List<Ebook> list(){
+    public CommonResp list(){
 
-        return ebookService.list();
+        CommonResp<List<Ebook>> resp = new CommonResp<>();
+
+        List<Ebook> list = ebookService.list();
+
+        resp.setContent(list);
+
+        return resp;
 
     }
 
