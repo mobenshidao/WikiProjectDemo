036c76a7d3577e8167f01929da2a1766b11539cf
jiahan.han <jiahan.han527@gmail.com>
2024-03-18 23:52:34 +0800

    3-6 creat ebook base and generate ebook mybatis code

--- doc/all.sql
+++ doc/all.sql
@@ -19,3 +19,25 @@ create table `demo`(
 )engine = innodb default charset = utf8mb4 comment = 'demo';
 
 insert into `demo` (id,name) value (1, 'test');
+
+
+#ebook
+drop table if exists `ebook`;
+create table `ebook`(
+                           `id` bigint not null comment 'id',
+                           `name` varchar(50) comment 'name',
+                           `category1_id` bigint comment 'category1',
+                           `category2_id` bigint comment 'category2',
+                           `description` varchar(200) comment 'description',
+                           `cover` varchar(200) comment 'cover',
+                           `doc_count` int comment 'doccount',
+                           `view_count` int comment 'viewcount',
+                           `vote_count` int comment 'votecount',
+                           primary key(`id`)
+)engine = innodb default charset = utf8mb4 comment = 'ebook';
+
+insert into `ebook` (id,name,description) value (1, 'Spring Boot guide','Guide you use Spring Boot');
+insert into `ebook` (id,name,description) value (2, 'Vue guide','Guide you use Vue');
+insert into `ebook` (id,name,description) value (3, 'Python guide','Best book teach you how to creat application with Python');
+insert into `ebook` (id,name,description) value (4, 'Guide of Mysql','How to use Mysql');
+insert into `ebook` (id,name,description) value (5, 'Master of Oracle','Lead you Learning of Oracle');
