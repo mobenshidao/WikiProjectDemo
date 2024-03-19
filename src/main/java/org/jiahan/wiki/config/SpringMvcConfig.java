5cfacf71dc23d6c63a6323c381189cfb38a30321
jiahan.han <jiahan.han527@gmail.com>
2024-03-19 20:33:04 +0800

    5-7 add interceptor and print time consume

---
+++ src/main/java/org/jiahan/wiki/config/SpringMvcConfig.java
@@ -0,0 +1,20 @@
+package org.jiahan.wiki.config;
+
+import org.jiahan.wiki.interceptor.LogInterceptor;
+import jakarta.annotation.Resource;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class SpringMvcConfig implements WebMvcConfigurer {
+
+    @Resource
+    LogInterceptor loginInterceptor;
+
+
+    public void addInterceptors(InterceptorRegistry registry) {
+        registry.addInterceptor(loginInterceptor)
+                .addPathPatterns("/**").excludePathPatterns("/login");
+    }
+}
