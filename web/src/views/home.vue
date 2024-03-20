bbc56a2f7e6cdb41a2feac9f528a457388ad8d89
jiahan.han <jiahan.han527@gmail.com>
2024-03-20 12:51:22 +0800

    6-6 homepage page spilt front and backend

--- web/src/views/home.vue
+++ web/src/views/home.vue
@@ -72,19 +72,19 @@
 import { defineComponent, onMounted,ref,reactive,toRef } from 'vue';
 import axios from 'axios';
 
-const listData: any = [];
-
-for (let i = 0; i < 23; i++) {
-  listData.push({
-    href: 'https://www.antdv.com/',
-    title: `ant design vue part ${i}`,
-    avatar: 'https://zos.alipayobjects.com/rmsportal/ODTLcjxAfvqbxHnVXCYX.png',
-    description:
-        'Ant Design, a design language for background applications, is refined by Ant UED Team.',
-    content:
-        'We supply a series of design principles, practical patterns and high quality design resources (Sketch and Axure), to help people create their product prototypes beautifully and efficiently.',
-  });
-}
+// const listData: any = [];
+//
+// for (let i = 0; i < 23; i++) {
+//   listData.push({
+//     href: 'https://www.antdv.com/',
+//     title: `ant design vue part ${i}`,
+//     avatar: 'https://zos.alipayobjects.com/rmsportal/ODTLcjxAfvqbxHnVXCYX.png',
+//     description:
+//         'Ant Design, a design language for background applications, is refined by Ant UED Team.',
+//     content:
+//         'We supply a series of design principles, practical patterns and high quality design resources (Sketch and Axure), to help people create their product prototypes beautifully and efficiently.',
+//   });
+// }
 
 export default defineComponent({
   name: 'Home',
@@ -97,10 +97,16 @@ export default defineComponent({
 
       // console.log("onMounted222");
 
-      axios.get("/ebook/list").then((response)=>{
+      axios.get("/ebook/list",{
+        params:{
+          page: 1,
+          size: 10000
+        }
+
+      }).then((response)=>{
         const data = response.data;
-        ebooks.value = data.content;
-        ebooks1.books = data.content;
+        ebooks.value = data.content.list;
+        //ebooks1.books = data.content;
         // console.log(response);
       });
     })
@@ -108,8 +114,8 @@ export default defineComponent({
     return {
 
       ebooks,
-      outBooks: toRef(ebooks1,"books"),
-      listData,
+      // outBooks: toRef(ebooks1,"books"),
+      // listData,
       pagination:{
 
         onChange: (page: any)=>{
